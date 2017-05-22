564c41be2df842fc502e9e0019759c3970245c87
GVadmin <gvadmin@BTRC.local>
2017-05-22 20:39:35 +0300

    + added settings hide js

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,5 +6,13 @@ import { Hero } from "./hero";
 export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
-    }
+    };
+
+    getHeroesSlowly(): Promise<Hero[]> {
+  return new Promise(resolve => {
+    // Simulate server latency with 2 second delay
+    setTimeout(() => resolve(this.getHeroes()), 2000);
+  });
+};
+
 }
