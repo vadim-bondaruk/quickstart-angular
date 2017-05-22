c885cac9986f37baf79ff1fae3c574b7ce9c55f2
GVadmin <gvadmin@BTRC.local>
2017-05-22 20:59:53 +0300

    + added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component'
+import { HeroesComponent } from "./heroes.component";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule ,
+  RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-dashboard',
+  template: '<h3>My Dashboard</h3>'
+})
+export class DashboardComponent { }
